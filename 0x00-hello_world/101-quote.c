5208630c6001dbce85a2694f1e19c16a6f5a87b3
brytemenz <74655254+brytemenz@users.noreply.github.com>
2022-09-23 11:15:25 +0000

    Add files via upload

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, "2015-10-19",
- * followed by a new line, to standard error.
+ * main - Entry point
  *
- * Return: Always 1 (success)
+ * Return: Always 1 (Success)
  */
+
 int main(void)
 {
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-return (1);
+	return (1);
 }
